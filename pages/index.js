b8391be3a47c4fa26db9233e2916282e3e5a0cd8
akshay <akshaybennygh@gmail.com>
2024-04-30 04:18:20 +0100

    edit

---
+++ pages/index.js
@@ -0,0 +1,138 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+	return (
+		<main className='bg-white font-serif space-y-[100px]'>
+			<section
+				id='hero'
+				className='px-[120px] bg-custom h-[70vh] w-full flex items-center justify-start'>
+				<div className='w-full max-w-[700px] space-y-[13px]'>
+					<h2 className='font-bold text-[36px] text-white'>
+						Richird Norton photorealistic rendering as real photos
+					</h2>
+					<p className='text-[12px] text-[#E5E5E5] max-w-[400px]'>
+						Progressively incentivize cooperative systems through
+						technically sound functionalities. The credibly
+						productivate seamless data.
+					</p>
+				</div>
+			</section>
+			<section
+				id='section-2'
+				className='px-[120px]'>
+				<h4 className='text-[#D4A373] font-bold text-[36px] pb-[24px]'>
+					&quot; Monotonectally seize superior mindshare rather than
+					efficient technology. &quot;
+				</h4>
+				<p className='text-[14px] text-[#495057] pb-5'>
+					Seamlessly syndicate cutting-edge architectures rather than
+					collaborative collaboration and idea-sharing. Proactively
+					incubate visionary interfaces whereas premium benefits.
+					Seamlessly negotiate ubiquitous leadership skills rather
+					than parallel ideas. Dramatically visualize superior
+					interfaces for best-of-breed alignments. Synergistically
+					formulate performance based users through customized
+					relationships. Interactively deliver cross-platform ROI via
+					granular systems. Intrinsicly enhance effective initiatives
+					vis-a-vis orthogonal outsourcing. Rapidiously monetize
+					market-driven opportunities with multifunctional users.
+					Collaboratively enhance visionary opportunities through
+					revolutionary schemas. Progressively network just in time
+					customer service without real-time scenarios.
+				</p>
+				<p className='text-[14px] text-[#495057] pb-5'>
+					Seamlessly syndicate cutting-edge architectures rather than
+					collaborative collaboration and idea-sharing. Proactively
+					incubate visionary interfaces whereas premium benefits.
+					Seamlessly negotiate ubiquitous leadership skills rather
+					than parallel ideas. Dramatically visualize superior
+					interfaces for best-of-breed alignments. Synergistically
+					formulate performance based users through customized
+					relationships. Interactively deliver cross-platform ROI via
+					granular systems. Intrinsicly enhance effective initiatives
+					vis-a-vis orthogonal outsourcing. Rapidiously monetize
+					market-driven opportunities with multifunctional users.
+					Collaboratively enhance visionary opportunities through
+					revolutionary schemas. Progressively network just in time
+					customer service without real-time scenarios.
+				</p>{' '}
+				<p className='text-[14px] text-[#495057]'>
+					Seamlessly syndicate cutting-edge architectures rather than
+					collaborative collaboration and idea-sharing. Proactively
+					incubate visionary interfaces whereas premium benefits.
+					Seamlessly negotiate ubiquitous leadership skills rather
+					than parallel ideas. Dramatically visualize superior
+					interfaces for best-of-breed alignments. Synergistically
+					formulate performance based users through customized
+					relationships. Interactively deliver cross-platform ROI via
+					granular systems. Intrinsicly enhance effective initiatives
+					vis-a-vis orthogonal outsourcing. Rapidiously monetize
+					market-driven opportunities with multifunctional users.
+					Collaboratively enhance visionary opportunities through
+					revolutionary schemas. Progressively network just in time
+					customer service without real-time scenarios.
+				</p>
+			</section>
+      <section
+				id='section-2'
+				className='px-[120px]'>
+				<h4 className='text-[#D4A373] font-bold text-[36px] pb-[24px]'>
+					&quot; Monotonectally seize superior mindshare rather than
+					efficient technology. &quot;
+				</h4>
+				<p className='text-[14px] text-[#495057] pb-5'>
+					Seamlessly syndicate cutting-edge architectures rather than
+					collaborative collaboration and idea-sharing. Proactively
+					incubate visionary interfaces whereas premium benefits.
+					Seamlessly negotiate ubiquitous leadership skills rather
+					than parallel ideas. Dramatically visualize superior
+					interfaces for best-of-breed alignments. Synergistically
+					formulate performance based users through customized
+					relationships. Interactively deliver cross-platform ROI via
+					granular systems. Intrinsicly enhance effective initiatives
+					vis-a-vis orthogonal outsourcing. Rapidiously monetize
+					market-driven opportunities with multifunctional users.
+					Collaboratively enhance visionary opportunities through
+					revolutionary schemas. Progressively network just in time
+					customer service without real-time scenarios.
+				</p>
+				<p className='text-[14px] text-[#495057] pb-5'>
+					Seamlessly syndicate cutting-edge architectures rather than
+					collaborative collaboration and idea-sharing. Proactively
+					incubate visionary interfaces whereas premium benefits.
+					Seamlessly negotiate ubiquitous leadership skills rather
+					than parallel ideas. Dramatically visualize superior
+					interfaces for best-of-breed alignments. Synergistically
+					formulate performance based users through customized
+					relationships. Interactively deliver cross-platform ROI via
+					granular systems. Intrinsicly enhance effective initiatives
+					vis-a-vis orthogonal outsourcing. Rapidiously monetize
+					market-driven opportunities with multifunctional users.
+					Collaboratively enhance visionary opportunities through
+					revolutionary schemas. Progressively network just in time
+					customer service without real-time scenarios.
+				</p>{' '}
+				<p className='text-[14px] text-[#495057]'>
+					Seamlessly syndicate cutting-edge architectures rather than
+					collaborative collaboration and idea-sharing. Proactively
+					incubate visionary interfaces whereas premium benefits.
+					Seamlessly negotiate ubiquitous leadership skills rather
+					than parallel ideas. Dramatically visualize superior
+					interfaces for best-of-breed alignments. Synergistically
+					formulate performance based users through customized
+					relationships. Interactively deliver cross-platform ROI via
+					granular systems. Intrinsicly enhance effective initiatives
+					vis-a-vis orthogonal outsourcing. Rapidiously monetize
+					market-driven opportunities with multifunctional users.
+					Collaboratively enhance visionary opportunities through
+					revolutionary schemas. Progressively network just in time
+					customer service without real-time scenarios.
+				</p>
+			</section>
+			<footer className='bg-[#212529] h-[30vh] w-full'></footer>
+		</main>
+	)
+}
